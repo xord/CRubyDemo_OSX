172bb7984e9af85cc51eb7cc39b7441ea4cc8faf
snori <snori@xord.org>
2014-12-21 21:20:42 +0900

    generated by Xcode.

---
+++ CRubyDemo_OSX/AppDelegate.m
@@ -0,0 +1,17 @@
+#import "AppDelegate.h"
+
+
+@interface AppDelegate ()
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification {
+}
+
+- (void)applicationWillTerminate:(NSNotification *)notification {
+}
+
+@end
---
+++ CRubyDemo_OSXTests/CRubyDemo_OSXTests.m
@@ -0,0 +1,28 @@
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+
+@interface CRubyDemo_OSXTests : XCTestCase
+@end
+
+
+@implementation CRubyDemo_OSXTests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testExample {
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    [self measureBlock:^{
+    }];
+}
+
+@end
