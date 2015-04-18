1c035387f8a0d202e66fcbaa669740bd00c95fdf
snori <snori@xord.org>
2015-04-19 05:25:49 +0900

    update CRuby to latest.

--- CRubyDemo_OSX/AppDelegate.m
+++ CRubyDemo_OSX/AppDelegate.m
@@ -10,7 +10,7 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification {
-	CRBValue *result = [CRuby eval:@"[1, 2, 3].map {|n| n ** 2}"];
+	CRBValue *result = [CRuby evaluate:@"[1, 2, 3].map {|n| n ** 2}"];
 	NSString *string = result.inspect;
 	NSLog(@"result: %@", string);
 }
